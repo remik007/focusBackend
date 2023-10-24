de90f913ba0cb03a432bbb0911d7035f4f248466
Remigiusz Majka <Remigiusz.Majka@h-f.co.uk>
2023-10-24 08:53:57 +0100

    Login Validator

---
+++ FocusAPI/Models/Validators/LoginDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FocusAPI.Data;
+
+namespace FocusAPI.Models.Validators
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(x => x.Login)
+                .NotEmpty();
+
+            RuleFor(x => x.Password)
+                .NotEmpty();
+        }
+    }
+}
