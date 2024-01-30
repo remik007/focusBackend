f881b41e8acc71365bb5c0f788cbf001c4f1c934
Remigiusz Majka <Remigiusz.Majka@h-f.co.uk>
2024-01-30 14:32:29 +0000

    max legth db migration

---
+++ FocusAPI/Data/Migrations/20240130143149_DescriptionMaxLength.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FocusAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DescriptionMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
