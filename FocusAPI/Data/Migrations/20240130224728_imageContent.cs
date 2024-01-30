e93f58c2a2fdd75387459e04f165435d943e2fb1
Remigiusz Majka <Remigiusz.Majka@h-f.co.uk>
2024-01-30 22:47:57 +0000

    image name, image content db migration

---
+++ FocusAPI/Data/Migrations/20240130224728_imageContent.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FocusAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class imageContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Trips",
+                newName: "ImageName");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "SubPages",
+                newName: "ImageName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageContent",
+                table: "Trips",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageContent",
+                table: "SubPages",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageContent",
+                table: "Trips");
+
+            migrationBuilder.DropColumn(
+                name: "ImageContent",
+                table: "SubPages");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageName",
+                table: "Trips",
+                newName: "ImageUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageName",
+                table: "SubPages",
+                newName: "ImageUrl");
+        }
+    }
+}
